b0b8720841ad4e6f8ac221dabc57311e19f7a967
Janon <janonguittard@gmail.com>
2025-05-25 03:19:59 +0200

    updates and install ux icons

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\UX\Icons\UXIconsBundle::class => ['all' => true],
 ];
