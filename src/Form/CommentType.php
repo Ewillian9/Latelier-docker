79b329351863a482a34a7f1b322b59963631bae4
Janon <janonguittard@gmail.com>
2025-05-31 21:56:33 +0200

    better comment form

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class CommentType extends AbstractType
@@ -11,14 +13,23 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
+            ->add('content', TextType::class, [
+            'attr' => [
+                'placeholder' => 'Type a message...',
+                'maxlength' => 100,
+            ],
+            'constraints' => [
+                new Length([
+                    'max' => 100,
+                    'maxMessage' => 'Cannot be longer than {{ limit }} characters',
+                ])
+            ],
+        ])
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            
-        ]);
+        $resolver->setDefaults([]);
     }
 }
